a80e03be48b636e7d30ce4040fae94141c97765c
tushardhankhar <tushardhankhar98@gmail.com>
2024-10-03 16:04:23 +0530

    Lint issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,6 +33,7 @@ export default function Header() {
     return () => {
       unsubscribe();
     };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   function handleSignOut(params) {
     signOut(auth)
