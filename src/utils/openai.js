4505908b83ad8733d5f22c3d1d28ee9051f7fc53
tushardhankhar <tushardhankhar98@gmail.com>
2024-10-14 15:12:38 +0530

    Test

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -1,5 +1,7 @@
 import OpenAI from "openai";
 
+console.log("api,",process.env.REACT_APP_OPENAPI_KEY)
+
 export const client = new OpenAI({
   apiKey: process.env.REACT_APP_OPENAPI_KEY,
   dangerouslyAllowBrowser: true,
